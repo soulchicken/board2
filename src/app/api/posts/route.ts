6e8cca808c567515eabbe95fbe4ba4b10e2deccc
soulchicken <soulfever01@naver.com>
2024-09-29 23:23:06 +0900

    fix: 타입

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
-import { Post } from '@/types';
 
 export async function GET() {
-  const { data, error } = await supabase.from<Post>('posts').select('*');
+  const { data, error } = await supabase.from('posts').select('*'); 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
